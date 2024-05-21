79aec60f9b75759036bb0036120801bddabbe67a
KmiloLopez <andres.lopez2018@outlook.es>
2024-05-21 17:30:03 -0500

    PORT

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import app from "./src/app.js";
-import { BPORT } from "./src/config.js";
+import { PORT } from "./src/config.js";
 import { connectDB } from "./src/db.js";
 
 connectDB();
-app.listen(BPORT, () => {
+app.listen(PORT, () => {
   console.log("listening on por port:4000");
 });
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
-export const BPORT = process.env.BPORT || "4000";
+export const PORT = process.env.PORT || "4000";
