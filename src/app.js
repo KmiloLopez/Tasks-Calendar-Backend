c8868f24564860053fb91744e667288e1e77431f
KmiloLopez <andres.lopez2018@outlook.es>
2024-05-24 15:51:03 -0500

    D

--- src/app.js
+++ src/app.js
@@ -29,11 +29,11 @@ app.use(taskRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "UserAuth-Front", "dist")));
+app.use(express.static(path.join(__dirname, "../UserAuth-Front", "dist")));
 
 // Redirige todas las solicitudes a `index.html`
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "UserAuth-Front", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../UserAuth-Front", "dist", "index.html"));
 });
 
 export default app;
